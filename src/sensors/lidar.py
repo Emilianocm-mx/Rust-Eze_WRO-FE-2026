97d6137d1e0fce9f1faab7f4ae9669f36293eefe
Emilianocm-mx <ecm4286@gmail.com>
2026-09-08 22:21:51 -0700

    Delete src/sensors directory

--- src/sensors/lidar.py
+++
@@ -1,9 +0,0 @@
-"""LIDAR / ultrasonic sensor interface."""
-
-class Lidar:
-    def __init__(self, config):
-        self.port = config.get("port", "/dev/ttyUSB0")
-
-    def scan(self) -> dict:
-        """Retorna distancias en mm para cada sector {front, left, right}."""
-        return {"front": 9999, "left": 9999, "right": 9999}
